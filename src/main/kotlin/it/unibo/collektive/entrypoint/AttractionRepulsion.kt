1f669ccb8be1ee65604cbf8781cde32ac6180c30
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-09-21 18:16:32 +0200

    feat: add base entrypoint for AttractionRepulsion exp

--- src/main/kotlin/it/unibo/collektive/entrypoint/AttractionRepulsion.kt
+++ src/main/kotlin/it/unibo/collektive/entrypoint/AttractionRepulsion.kt
@@ -1,4 +1,4 @@
-package it.unibo.collektive.examples.gradient
+package it.unibo.collektive.entrypoint
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.Field
@@ -12,21 +12,11 @@ import kotlin.Double.Companion.POSITIVE_INFINITY
 /**
  * Given the [distances] from the source, this function computes the gradient from the [source] to self.
  */
-fun Aggregate<Int>.gradient(distances: Field<Int, Double>, source: Boolean): Double =
-    share(POSITIVE_INFINITY) { field ->
-        val minGradient = (field + distances).neighbors.values.min()
-        when {
-            source -> 0.0
-            else -> minGradient
-        }
-    }
+fun <ID : Comparable<ID>> Aggregate<ID>.attractionRepulsion(): Double = TODO()
 
 /**
  * The entrypoint of the simulation running a gradient, considering the device with id 0 as the source.
  */
-fun Aggregate<Int>.gradientEntrypoint(distanceSensor: CollektiveDevice<*>): Double = with(distanceSensor) {
-    gradient(
-        distances = distances(),
-        source = localId == 0,
-    )
+fun Aggregate<Int>.attractionRepulsionEntrypoint(distanceSensor: CollektiveDevice<*>): Double = with(distanceSensor) {
+    TODO()
 }
