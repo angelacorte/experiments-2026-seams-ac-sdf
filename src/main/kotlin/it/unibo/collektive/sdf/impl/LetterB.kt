136f3acf02611cfe7b4bd92992aded01136d17c3
Angela Cortecchia <angela.cortecchia@unibo.it>
2026-09-22 12:12:07 +0200

    refactor: rename variables for clarity in LetterB class

--- src/main/kotlin/it/unibo/collektive/sdf/impl/LetterB.kt
+++ src/main/kotlin/it/unibo/collektive/sdf/impl/LetterB.kt
@@ -11,46 +11,60 @@ import kotlin.math.PI
  * @param height The total height of the letter.
  * @property thickness The thickness of the letter's strokes (default is 0.0).
  */
-class LetterB(start: Position, height: Double, private val thickness: Double = 0.0) : SDF {
-    private val vertical = Segment(start, Position(start.x, start.y + height))
-    private val lowArc =
-        Arc(
-            Position(start.x + height / QUARTER_DIVISOR, start.y + height / QUARTER_DIVISOR),
-            height / QUARTER_DIVISOR,
-            -PI / HALF_DIVISOR,
-            PI,
-        )
-    private val highArc =
-        Arc(
-            Position(start.x + height / EIGHTH_DIVISOR, start.y + height * THREE_QUARTERS),
-            height / QUARTER_DIVISOR,
-            -PI / HALF_DIVISOR,
-            PI,
-        )
-    private val lowSeg = Segment(start, Position(start.x + height / QUARTER_DIVISOR, start.y))
-    private val midSeg =
-        Segment(
-            Position(start.x, start.y + height / HALF_DIVISOR),
-            Position(start.x + height / QUARTER_DIVISOR, start.y + height / HALF_DIVISOR),
-        )
-    private val highSeg =
-        Segment(
-            Position(start.x, start.y + height),
-            Position(start.x + height / EIGHTH_DIVISOR, start.y + height),
-        )
+class LetterB(
+    start: Position,
+    height: Double,
+    private val thickness: Double = 0.0,
+) : SDF {
+    private val verticalStem = Segment(start, Position(start.x, start.y + height))
+
+    private val lowerArc = Arc(
+        Position(
+            start.x + height / QUARTER_DIVISOR,
+            start.y + height / QUARTER_DIVISOR,
+        ),
+        height / QUARTER_DIVISOR,
+        -PI / HALF_DIVISOR,
+        PI,
+    )
+
+    private val upperArc = Arc(
+        Position(
+            start.x + height / EIGHTH_DIVISOR,
+            start.y + height * THREE_QUARTERS,
+        ),
+        height / QUARTER_DIVISOR,
+        -PI / HALF_DIVISOR,
+        PI,
+    )
+
+    private val lowerSegment = Segment(
+        start,
+        Position(start.x + height / QUARTER_DIVISOR, start.y),
+    )
+
+    private val middleSegment = Segment(
+        Position(start.x, start.y + height / HALF_DIVISOR),
+        Position(
+            start.x + height / QUARTER_DIVISOR,
+            start.y + height / HALF_DIVISOR,
+        ),
+    )
+
+    private val upperSegment = Segment(
+        Position(start.x, start.y + height),
+        Position(start.x + height / EIGHTH_DIVISOR, start.y + height),
+    )
 
     override fun invoke(position: Position): Double = minOf(
-        vertical(position),
-        highArc(position),
-        lowArc(position),
-        lowSeg(position),
-        midSeg(position),
-        highSeg(position),
+        verticalStem(position),
+        upperArc(position),
+        lowerArc(position),
+        lowerSegment(position),
+        middleSegment(position),
+        upperSegment(position),
     ) - thickness
 
-    /**
-     * Constants used for proportional geometry calculations.
-     */
     companion object {
         private const val HALF_DIVISOR = 2.0
         private const val QUARTER_DIVISOR = 4.0
